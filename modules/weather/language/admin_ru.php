836803820a96051d5bd34204ced7d634e597ccf6
NukeVlad <vipvnnru@yahoo.com>
2020-12-03 11:47:03 +0300

    Add files via upload
    Добавление русской локализации

---
+++ modules/weather/language/admin_ru.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @Project Weather 4.3.00
+ * @Author Thuc Vinh (tichtacso.com@gmail.com)
+ * @Copyright (C) 2017 Tich Tac So. All rights reserved
+ * @Createdate 22/12/2017, 15:20
+ */
+
+if ( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
+
+$lang_translator['author'] = 'Hậu Phạm (phamdinhhau@gmail.com)';
+$lang_translator['createdate'] = '23/5/2014, 15:23';
+$lang_translator['copyright'] = '@Copyright (C) 2014 Hau\'s Corp. All rights reserved';
+$lang_translator['info'] = '';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['list'] = 'Список провинций / городов';
+$lang_module['add'] = 'Добавить новый';
+$lang_module['edit'] = 'Редактировать';
+$lang_module['save'] = 'Сохранить';
+$lang_module['order'] = 'Порядок';
+$lang_module['status'] = 'Статус';
+$lang_module['active'] = 'Активно';
+$lang_module['inactive'] = 'Не активно';
+$lang_module['feature'] = 'Функция';
+$lang_module['location_name'] = 'Область / Город';
+$lang_module['location_code'] = 'Код';
+$lang_module['setting'] = 'Конфигурация';
+$lang_module['api'] = 'API';
+$lang_module['error_required_location_name'] = 'Ошибка: вам нужно ввести данные для области / города';
+$lang_module['error_required_location_code'] = 'Ошибка: вам нужно ввести код города/обасти';
---
+++ modules/weather/language/ru.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC <contact@vinades.vn>
+ * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
+ * @Language Tiếng Việt
+ * @License CC BY-SA (http://creativecommons.org/licenses/by-sa/4.0/)
+ * @Createdate Mar 04, 2010, 03:22:00 PM
+ */
+
+if (!defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$lang_translator['author'] = 'VINADES.,JSC <contact@vinades.vn>';
+$lang_translator['createdate'] = '04/03/2010, 15:22';
+$lang_translator['copyright'] = '@Copyright (C) 2012 VINADES.,JSC. All rights reserved';
+$lang_translator['info'] = '';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['location'] = 'Область / город';
+$lang_module['min_max_temp'] = 'Самая низкая / максимальная температура';
+$lang_module['wind'] = 'Ветер';
+$lang_module['humidity'] = 'Влажность';
+$lang_module['today_weather'] = 'Погода сегодня';
+$lang_module['sunrise'] = 'Восход солнца';
+$lang_module['sunset'] = 'Закат';
+$lang_module['api_error'] = 'Отсутствующий или неправильный API';
